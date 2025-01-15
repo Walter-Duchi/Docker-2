eec145094e172ce9ee017b35d94eeba59d684aa7
Walter-Duchi <waltduchi@gmail.com>
2025-01-15 14:25:52 -0500

    created docker build

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 import express from 'express'
-import mysql from 'mysql2/promise'
-import mongoose from 'mongoose'
+
 
 const app = express()
 
-const client = mysql.createPool({
-  host: 'localhost',
-  port: 2000,
-  user: 'root',
-  password: 'wadr'
+app.get('/', (req, res)=>{
+  res.send("Hello World")
 })
-
-const response = await client.query('SELECT 1+1')
-
-console.log(response[0])
-
-const mongoConnection = await mongoose.connect('mongodb://localhost:27017/wadrdb')
-console.log(mongoConnection.connection.db.databaseName)
 
 app.listen(3000)
 console.log('Server on port 3000')
