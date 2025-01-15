9ccca0f6c283aa98c637525f4084b8754a6645e0
Walter-Duchi <waltduchi@gmail.com>
2025-01-15 20:16:33 +0000

    Added uuid dependency and updated server.js to return a UUID on the root route

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express'
-
+import { v4 } from 'uuid'
 
 const app = express()
 
-app.get('/', (req, res)=>{
-  res.send("Hello World")
+app.get('/', (req, res) => {
+  res.json({
+    id: v4()
+  })
 })
 
 app.listen(3000)
